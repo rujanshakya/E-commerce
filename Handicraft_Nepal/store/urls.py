cc848ef4bdfc0320d8950415d6dcd5c569375e0e
rujanshakya <rujan.shakya123@gmail.com>
2022-03-07 17:08:02 +0545

    Revert "Revert "Created  Bootstrap framework""
    This reverts commit e7c3b9ae67ea44b9e0d0bfd8986cbce349dac493.

--- Handicraft_Nepal/store/urls.py
+++ Handicraft_Nepal/store/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('about/',views.aboutPage,name='about'),
+    path('product/',views.productPage,name='product'),
 ]
--- Handicraft_Nepal/store/views.py
+++ Handicraft_Nepal/store/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"home.html")
-def aboutPage(request):
-    return render(request,"about.html")
+def productPage(request):
+    return render(request,"product.html")
